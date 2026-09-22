f6a3ab5e7229304476ac74ed8e9938b43cfd9f7d
samparkadas6 <samparkadas6@gmail.com>
2026-09-23 00:03:09 +0530

    Add files via upload

---
+++ binary.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int binarysearch(int array[],int x,int low,int high){
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if (x == array[mid]){
+            return mid;
+        }
+        if(x>array[mid]){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+int main(){
+    int array[] = {10,15,20,25,30,35,40,45,50,55,60};
+    int low = 0;
+    int high = 11;
+    int x = 45;
+    int result = binarysearch(array,x,0,high);
+    if (result == -1){
+        printf("not found");
+    }
+    else{
+        printf("found at %d",result);
+    }
+    return 0;
+}
