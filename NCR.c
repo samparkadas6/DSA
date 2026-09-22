d778ffc1a20b54af106dd669dcecf50d3243d4dd
samparkadas6 <samparkadas6@gmail.com>
2026-09-23 00:16:57 +0530

    Add files via upload

---
+++ NCR.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int factorial(int x){
+    int fact = 1;
+    for (int i = 2;i<=x;i++){
+        fact = fact*i;
+    }
+    return fact;
+}
+int main(){
+    int n;
+    printf("enter the valua of n");
+    scanf("%d",&n);
+    int r ;
+    printf("enter the r");
+    scanf("%d",&r);
+    int ncr = factorial(n)/(factorial(r)*factorial(n-r));
+    printf("%d",ncr);
+    return 0;
+}
